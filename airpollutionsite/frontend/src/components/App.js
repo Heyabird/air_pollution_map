a59ff6ae114231ae54b9efb2b63e0e2efabe54f9
heya <heya@evealed.com>
2020-05-17 13:37:43 -0700

    moved the line chart into timeSeriesChart.js

--- airpollutionsite/frontend/src/components/App.js
+++ airpollutionsite/frontend/src/components/App.js
@@ -31,108 +31,6 @@ class App extends React.Component {
       zoom: this.state.zoom
       });
       
-    // map.on('load', () => {
-    //   console.log("loading")
-    //   map.addSource('points', {
-    //     'type': 'geojson',
-    //       'data': {
-    //       'type': 'FeatureCollection',
-    //       'features': [
-    //         {
-    //           'type': 'Feature',
-    //           'geometry': {
-    //           'type': 'Point',
-    //           'coordinates': [
-    //             -117.228993, 32.866037
-    //             ]
-    //           },
-    //           'properties': {
-    //             'title': 'SD',
-    //             'icon': 'harbor ',
-    //             'description':
-    //               '<p>San Diego</p>'
-    //           }
-    //         },
-    //         {
-    //           'type': 'Feature',
-    //           'geometry': {
-    //             'type': 'Point',
-    //             'coordinates': [-122.414, 37.776]
-    //             },
-    //           'properties': {
-    //             'title': 'SF',
-    //             'icon': 'harbor',
-    //             'description':
-    //               '<p>San Francisco.</p>'
-    //           }
-    //         },
-    //         {
-    //           'type': 'Feature',
-    //           'geometry': {
-    //             'type': 'Point',
-    //             'coordinates': [-118.2437, 34.0522]
-    //             },
-    //           'properties': {
-    //             'title': 'LA',
-    //             'icon': 'harbor',
-    //             'description':
-    //               '<p>Los Angeles</p>'
-    //           }
-    //         }
-    //       ]
-    //     }
-    //   });
-  
-    //   map.addLayer({
-    //     'id': 'points',
-    //     'type': 'symbol',
-    //     'source': 'points',
-    //       'layout': {
-    //       // get the icon name from the source's "icon" property
-    //       // concatenate the name to get an icon from the style's sprite sheet
-    //       'icon-image': ['concat', ['get', 'icon'], '-15'],
-    //       'icon-size': 2,
-    //       // get the title name from the source's "title" property
-    //       'text-field': ['get', 'title'],
-    //       'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-    //       'text-offset': [0, 0.6],
-    //       'text-anchor': 'top'
-    //       }
-    //   });
-
-    //   var popup = new mapboxgl.Popup({
-    //     closeButton: false,
-    //     closeOnClick: false
-    //     });
-
-    //   map.on('mouseenter', 'points', (e) => {
-    //     var coordinates = e.features[0].geometry.coordinates.slice();
-    //     var description = e.features[0].properties.description;
-
-    //     map.getCanvas().style.cursor = 'pointer';
- 
-    //     var coordinates = e.features[0].geometry.coordinates.slice();
-    //     var description = e.features[0].properties.description;
-
-    //     // Ensure that if the map is zoomed out such that multiple
-    //     // copies of the feature are visible, the popup appears
-    //     // over the copy being pointed to.
-    //     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-    //     coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    //     }
-      
-    //     popup
-    //       .setLngLat(coordinates)
-    //       .setHTML(description)
-    //       .addTo(map)
-    //       .setMaxWidth("400px")
-    //   });
-
-    //   map.on('mouseleave', 'points', function() {
-    //     map.getCanvas().style.cursor = '';
-    //     popup.remove();
-    //     });
-    // }) 
   }
 
   test() {
@@ -145,6 +43,7 @@ class App extends React.Component {
       console.log(err)
     })
   }
+  
   
   componentDidMount() {
     this.mapSetUp(); 
--- airpollutionsite/frontend/src/components/timeSeriesChart.js
+++ airpollutionsite/frontend/src/components/timeSeriesChart.js
@@ -3,49 +3,53 @@ import React from "react";
 class TimeSeriesChart extends React.Component {
 	// const myChartRef = this.chartRef.current.getContext("2d");
 	
-	// componentDidMount() {
-	//   var ctx = document.getElementById('chart').getContext('2d');
-	// 	var  = new Chart(ctx, {
-	// 	type: 'bar',
-	// 	data: {
-	// 		labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-	// 		datasets: [{
-	// 			label: '# of Votes',
-	// 			data: [12, 19, 3, 5, 2, 3],
-	// 			backgroundColor: [
-	// 				'rgba(255, 99, 132, 0.2)',
-	// 				'rgba(54, 162, 235, 0.2)',
-	// 				'rgba(255, 206, 86, 0.2)',
-	// 				'rgba(75, 192, 192, 0.2)',
-	// 				'rgba(153, 102, 255, 0.2)',
-	// 				'rgba(255, 159, 64, 0.2)'
-	// 			],
-	// 			borderColor: [
-	// 				'rgba(255, 99, 132, 1)',
-	// 				'rgba(54, 162, 235, 1)',
-	// 				'rgba(255, 206, 86, 1)',
-	// 				'rgba(75, 192, 192, 1)',
-	// 				'rgba(153, 102, 255, 1)',
-	// 				'rgba(255, 159, 64, 1)'
-	// 			],
-	// 			borderWidth: 1
-	// 		}]
-	// 	},
-	// 	options: {
-	// 		scales: {
-	// 			yAxes: [{
-	// 				ticks: {
-	// 					beginAtZero: true
-  //         }
-  //       }]
-  //     }
-  //   }
-  //   });
-  // }
+	componentDidMount() {
+    const ctx = document.getElementById('chart').getContext('2d');
+    const xLabels = [2018, 2019, 2020];
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xLabels,
+            datasets: [{
+                label: 'pm2.5',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+  }
+
 	render() {
     return (
 			<>
 				<div id="box">Time Series Chart</div>
+        <canvas id="chart" width="400" height="400"></canvas>
+
 			</>		
 	)
 	}
